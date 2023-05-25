1751dbf4fe61739fa82476c98e7be5df79d15c22
srira <srira@LAPTOP-14U66787>
2023-05-25 18:22:23 +0530

    new lap

--- testGitDemo/src/test/java/com/test/LoginTest.java
+++ testGitDemo/src/test/java/com/test/LoginTest.java
@@ -4,7 +4,7 @@ public class LoginTest {
 	public void loginTest() {
 		System.out.println("Test login page");
 		System.out.println("Test 34674567");
-		System.out.println("test sdfgsdfg"));
+		System.out.println("test sdfgsdfg")); 
 		
 	}
 }
